be88ba541fcc813fe78aea11bf57e711ad4be655
Chatchai Thanatammatat <163982803+CCthana@users.noreply.github.com>
2024-06-24 10:10:12 +0700

    Update route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -8,54 +8,12 @@ import Slide from "../components/Slide";
 // const HomePage = lazy(() => import('../pages/HomePage'));
 // const MainContainer = lazy(() => import('../layout/MainContainer'));
 
-const mockData = [
-  {
-    imgSrc: "https://picsum.photos/1920/540?random=1",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
-    link: {
-      linkSrc: "/hospital/1",
-      button: "line: โรงพยาบาล 1",
-    },
-  },
-  {
-    imgSrc: "https://picsum.photos/1920/540?random=2",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 2",
-    link: {
-      linkSrc: "/hospital/2",
-      button: "line: โรงพยาบาล 2",
-    },
-  },
-  {
-    imgSrc: "https://picsum.photos/1920/540?random=3",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 3",
-    link: {
-      linkSrc: "/hospital/3",
-      button: "line: โรงพยาบาล 3",
-    },
-  },
-  {
-    imgSrc: "https://picsum.photos/1920/540?random=4",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 4",
-    link: {
-      linkSrc: "/hospital/4",
-      button: "line: โรงพยาบาล 4",
-    },
-  },
-  {
-    imgSrc: "https://picsum.photos/1920/540?random=5",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 5",
-    link: {
-      linkSrc: "/hospital/5",
-      button: "line: โรงพยาบาล 5",
-    },
-  },
-];
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <Slide slides={mockData} width="full" height="96" fit="object-contain" />
+     <h1>helloooooo</h1>
     ),
   },
   { path: "/user", element: <UserAllVn /> },
