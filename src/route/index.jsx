0c570bb6109e5b05d0b7c46673ce5cf1e0144d15
CCthana <c.thanatammatat@gmail.com>
2024-06-25 18:12:28 +0700

    admin page and route

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -10,7 +10,6 @@ import AdminNursePage from '../features/admin/page/AdminNursePage';
 import AdminDoctorPage from '../features/admin/page/AdminDoctorPage';
 import AdminAccountPage from '../features/admin/page/AdminAccountPage';
 import AdminAppointment from '../features/admin/page/AdminAppointment';
-import AdminSearchHn from '../features/admin/component/AdminSearchHn';
 import AdminLanding from '../features/admin/page/AdminLanding';
 
 const router = createBrowserRouter([
@@ -22,15 +21,20 @@ const router = createBrowserRouter([
       {path: '/user',element: <UserAllVn />},
       {path: '/user/appointment',element: <UserAppointment />},
       {path: '/user/setting',element: <UserSetting />},
+
+
+
+      {path: '/admin',element: <AdminLanding />},
       {path: '/admin/createvn',element: <VnCreateAdmin />},
       {path: '/admin/vnconfirm',element: <VnConfirmAdmin />},
       {path: '/admin/nurse',element: <AdminNursePage />},
       {path: '/admin/doctor',element: <AdminDoctorPage />},
       {path: '/admin/account',element: <AdminAccountPage />},
-      {path: '/admin',element: <AdminLanding />},
+      {path: '/admin/appointment',element: <AdminAppointment />},
+         
+      
 
-      // for modal in createvn
-      {path: '/admin/appoint',element: <AdminAppointment />},
+   
       
 
 
